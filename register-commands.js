66fa01ac7b6fde4a83b41cd8ca90f40347201d94
Machi2603 <machito2603@gmail.com>
2025-12-30 01:17:21 +0100

    Add '/censura' command alongside '/willybot'

--- register-commands.js
+++ register-commands.js
@@ -1,7 +1,8 @@
 import 'dotenv/config';
 import { REST, Routes, SlashCommandBuilder } from 'discord.js';
 
-const command = new SlashCommandBuilder()
+// /willybot texto: "..."
+const willybotCommand = new SlashCommandBuilder()
   .setName('willybot')
   .setDescription('habla con willybot')
   .addStringOption(opt =>
@@ -12,14 +13,24 @@ const command = new SlashCommandBuilder()
       .setMaxLength(2000)
   );
 
+// /censura activada: true/false
+const censuraCommand = new SlashCommandBuilder()
+  .setName('censura')
+  .setDescription('Activa o desactiva la censura de Willybot')
+  .addBooleanOption(opt =>
+    opt.setName('activada')
+      .setDescription('Si: Censura activada. No: Censura desactivada')
+      .setRequired(true)
+  );
+
 const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_BOT_TOKEN);
 
 async function main() {
   await rest.put(
-    Routes.applicationCommands(process.env.CLIENT_ID), // <-- GLOBAL
-    { body: [command.toJSON()] }
+    Routes.applicationCommands(process.env.CLIENT_ID), // GLOBAL
+    { body: [willybotCommand.toJSON(), censuraCommand.toJSON()] }
   );
-  console.log('Slash command GLOBAL registrado.');
+  console.log('Slash commands GLOBAL registrados: /willybot y /censura');
 }
 
 main().catch(console.error);
