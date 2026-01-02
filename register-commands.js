829eec52230cb3f34c4b42e414d58af5cd655c9a
Machi2603 <machito2603@gmail.com>
2026-01-02 20:10:09 +0100

    Add willytts command to register-commands.js

--- register-commands.js
+++ register-commands.js
@@ -1,7 +1,7 @@
 import 'dotenv/config';
 import { REST, Routes, SlashCommandBuilder } from 'discord.js';
 
-// /willybot texto: "..."
+// /willybot texto
 const willybotCommand = new SlashCommandBuilder()
   .setName('willybot')
   .setDescription('habla con willybot')
@@ -13,7 +13,7 @@ const willybotCommand = new SlashCommandBuilder()
       .setMaxLength(2000)
   );
 
-// /censura activada: true/false
+// /censura activada
 const censuraCommand = new SlashCommandBuilder()
   .setName('censura')
   .setDescription('Activa o desactiva la censura de Willybot')
@@ -23,14 +23,26 @@ const censuraCommand = new SlashCommandBuilder()
       .setRequired(true)
   );
 
+// /willytts mensaje
+const willyttsCommand = new SlashCommandBuilder()
+  .setName('willytts')
+  .setDescription('Lee un mensaje con voz usando ElevenLabs')
+  .addStringOption(opt =>
+    opt.setName('mensaje')
+      .setDescription('Texto que WillyBot dirá en voz alta')
+      .setRequired(true)
+      .setMinLength(1)
+      .setMaxLength(500)
+  );
+
 const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_BOT_TOKEN);
 
 async function main() {
   await rest.put(
     Routes.applicationCommands(process.env.CLIENT_ID), // GLOBAL
-    { body: [willybotCommand.toJSON(), censuraCommand.toJSON()] }
+    { body: [willybotCommand.toJSON(), censuraCommand.toJSON(), willyttsCommand.toJSON()] }
   );
-  console.log('Slash commands GLOBAL registrados: /willybot y /censura');
+  console.log('Slash commands GLOBAL registrados: /willybot, /censura y /willytts');
 }
 
 main().catch(console.error);
